60a0dec0e132f7bb97225c4ffc442779a59f5d71
Kittywhiskers Van Gogh <63189531+kittywhiskers@users.noreply.github.com>
2021-11-21 15:56:53 +0530

    hsmodule: move scales and chords to menus and separate the two

---
+++ menus/keys/chords.lua
@@ -0,0 +1,212 @@
+function Octaves()
+    hs.eventtap.keyStroke({"cmd"}, "C", 0);
+    hs.eventtap.keyStroke({"cmd"}, "V", 0)
+    for i = 1, 12, 1 do
+        hs.eventtap.keyStroke({}, "Up", 0)
+    end
+end
+
+function Powerchord()
+    hs.eventtap.keyStroke({"cmd"}, "C", 0);
+    hs.eventtap.keyStroke({"cmd"}, "V", 0)
+    for i = 1, 7, 1 do
+        hs.eventtap.keyStroke({}, "Up", 0)
+    end
+end
+
+function Maj()
+    hs.eventtap.keyStroke({"cmd"}, "C", 0);
+    hs.eventtap.keyStroke({"cmd"}, "V", 0)
+    for i = 1, 4, 1 do
+        hs.eventtap.keyStroke({}, "Up", 0)
+    end
+    hs.eventtap.keyStroke({"cmd"}, "C", 0);
+    hs.eventtap.keyStroke({"cmd"}, "V", 0)
+    for i = 1, 3, 1 do
+        hs.eventtap.keyStroke({}, "Up", 0)
+    end
+end
+
+function Min()
+    hs.eventtap.keyStroke({"cmd"}, "C", 0);
+    hs.eventtap.keyStroke({"cmd"}, "V", 0)
+    for i = 1, 3, 1 do
+        hs.eventtap.keyStroke({}, "Up", 0)
+    end
+    hs.eventtap.keyStroke({"cmd"}, "C", 0);
+    hs.eventtap.keyStroke({"cmd"}, "V", 0)
+    for i = 1, 4, 1 do
+        hs.eventtap.keyStroke({}, "Up", 0)
+    end
+end
+
+function Aug()
+    hs.eventtap.keyStroke({"cmd"}, "C", 0);
+    hs.eventtap.keyStroke({"cmd"}, "V", 0)
+    for i = 1, 4, 1 do
+        hs.eventtap.keyStroke({}, "Up", 0)
+    end
+    hs.eventtap.keyStroke({"cmd"}, "C", 0);
+    hs.eventtap.keyStroke({"cmd"}, "V", 0)
+    for i = 1, 4, 1 do
+        hs.eventtap.keyStroke({}, "Up", 0)
+    end
+end
+
+function Dim()
+    hs.eventtap.keyStroke({"cmd"}, "C", 0);
+    hs.eventtap.keyStroke({"cmd"}, "V", 0)
+    for i = 1, 3, 1 do
+        hs.eventtap.keyStroke({}, "Up", 0)
+    end
+    hs.eventtap.keyStroke({"cmd"}, "C", 0);
+    hs.eventtap.keyStroke({"cmd"}, "V", 0)
+    for i = 1, 3, 1 do
+        hs.eventtap.keyStroke({}, "Up", 0)
+    end
+end
+
+function Maj7()
+    hs.eventtap.keyStroke({"cmd"}, "C", 0);
+    hs.eventtap.keyStroke({"cmd"}, "V", 0)
+    for i = 1, 4, 1 do
+        hs.eventtap.keyStroke({}, "Up", 0)
+    end
+    hs.eventtap.keyStroke({"cmd"}, "C", 0);
+    hs.eventtap.keyStroke({"cmd"}, "V", 0)
+    for i = 1, 3, 1 do
+        hs.eventtap.keyStroke({}, "Up", 0)
+    end
+    hs.eventtap.keyStroke({"cmd"}, "C", 0);
+    hs.eventtap.keyStroke({"cmd"}, "V", 0)
+    for i = 1, 4, 1 do
+        hs.eventtap.keyStroke({}, "Up", 0)
+    end
+end
+
+function Min7()
+    hs.eventtap.keyStroke({"cmd"}, "C", 0);
+    hs.eventtap.keyStroke({"cmd"}, "V", 0)
+    for i = 1, 3, 1 do
+        hs.eventtap.keyStroke({}, "Up", 0)
+    end
+    hs.eventtap.keyStroke({"cmd"}, "C", 0);
+    hs.eventtap.keyStroke({"cmd"}, "V", 0)
+    for i = 1, 4, 1 do
+        hs.eventtap.keyStroke({}, "Up", 0)
+    end
+    hs.eventtap.keyStroke({"cmd"}, "C", 0);
+    hs.eventtap.keyStroke({"cmd"}, "V", 0)
+    for i = 1, 3, 1 do
+        hs.eventtap.keyStroke({}, "Up", 0)
+    end
+end
+
+function Dom7()
+    hs.eventtap.keyStroke({"cmd"}, "C", 0);
+    hs.eventtap.keyStroke({"cmd"}, "V", 0)
+    for i = 1, 4, 1 do
+        hs.eventtap.keyStroke({}, "Up", 0)
+    end
+    hs.eventtap.keyStroke({"cmd"}, "C", 0);
+    hs.eventtap.keyStroke({"cmd"}, "V", 0)
+    for i = 1, 3, 1 do
+        hs.eventtap.keyStroke({}, "Up", 0)
+    end
+    hs.eventtap.keyStroke({"cmd"}, "C", 0);
+    hs.eventtap.keyStroke({"cmd"}, "V", 0)
+    for i = 1, 3, 1 do
+        hs.eventtap.keyStroke({}, "Up", 0)
+    end
+end
+
+function Maj9()
+    hs.eventtap.keyStroke({"cmd"}, "C", 0);
+    hs.eventtap.keyStroke({"cmd"}, "V", 0)
+    for i = 1, 4, 1 do
+        hs.eventtap.keyStroke({}, "Up", 0)
+    end
+    hs.eventtap.keyStroke({"cmd"}, "C", 0);
+    hs.eventtap.keyStroke({"cmd"}, "V", 0)
+    for i = 1, 3, 1 do
+        hs.eventtap.keyStroke({}, "Up", 0)
+    end
+    hs.eventtap.keyStroke({"cmd"}, "C", 0);
+    hs.eventtap.keyStroke({"cmd"}, "V", 0)
+    for i = 1, 4, 1 do
+        hs.eventtap.keyStroke({}, "Up", 0)
+    end
+    hs.eventtap.keyStroke({"cmd"}, "C", 0);
+    hs.eventtap.keyStroke({"cmd"}, "V", 0)
+    for i = 1, 3, 1 do
+        hs.eventtap.keyStroke({}, "Up", 0)
+    end
+end
+
+function Min9()
+    hs.eventtap.keyStroke({"cmd"}, "C", 0);
+    hs.eventtap.keyStroke({"cmd"}, "V", 0)
+    for i = 1, 3, 1 do
+        hs.eventtap.keyStroke({}, "Up", 0)
+    end
+    hs.eventtap.keyStroke({"cmd"}, "C", 0);
+    hs.eventtap.keyStroke({"cmd"}, "V", 0)
+    for i = 1, 4, 1 do
+        hs.eventtap.keyStroke({}, "Up", 0)
+    end
+    hs.eventtap.keyStroke({"cmd"}, "C", 0);
+    hs.eventtap.keyStroke({"cmd"}, "V", 0)
+    for i = 1, 3, 1 do
+        hs.eventtap.keyStroke({}, "Up", 0)
+    end
+    hs.eventtap.keyStroke({"cmd"}, "C", 0);
+    hs.eventtap.keyStroke({"cmd"}, "V", 0)
+    for i = 1, 4, 1 do
+        hs.eventtap.keyStroke({}, "Up", 0)
+    end
+end
+
+function Fold3()
+    hs.eventtap.keyStroke({"cmd"}, "C", 0);
+    hs.eventtap.keyStroke({"cmd"}, "V", 0);
+    hs.eventtap.keyStroke({}, "Up", 0);
+    hs.eventtap.keyStroke({}, "Up", 0)
+    hs.eventtap.keyStroke({"cmd"}, "C", 0);
+    hs.eventtap.keyStroke({"cmd"}, "V", 0);
+    hs.eventtap.keyStroke({}, "Up", 0);
+    hs.eventtap.keyStroke({}, "Up", 0)
+end
+
+function Fold7()
+    hs.eventtap.keyStroke({"cmd"}, "C", 0);
+    hs.eventtap.keyStroke({"cmd"}, "V", 0);
+    hs.eventtap.keyStroke({}, "Up", 0);
+    hs.eventtap.keyStroke({}, "Up", 0)
+    hs.eventtap.keyStroke({"cmd"}, "C", 0);
+    hs.eventtap.keyStroke({"cmd"}, "V", 0);
+    hs.eventtap.keyStroke({}, "Up", 0);
+    hs.eventtap.keyStroke({}, "Up", 0)
+    hs.eventtap.keyStroke({"cmd"}, "C", 0);
+    hs.eventtap.keyStroke({"cmd"}, "V", 0);
+    hs.eventtap.keyStroke({}, "Up", 0);
+    hs.eventtap.keyStroke({}, "Up", 0)
+end
+
+function Fold9()
+    hs.eventtap.keyStroke({"cmd"}, "C", 0);
+    hs.eventtap.keyStroke({"cmd"}, "V", 0);
+    hs.eventtap.keyStroke({}, "Up", 0);
+    hs.eventtap.keyStroke({}, "Up", 0)
+    hs.eventtap.keyStroke({"cmd"}, "C", 0);
+    hs.eventtap.keyStroke({"cmd"}, "V", 0);
+    hs.eventtap.keyStroke({}, "Up", 0);
+    hs.eventtap.keyStroke({}, "Up", 0)
+    hs.eventtap.keyStroke({"cmd"}, "C", 0);
+    hs.eventtap.keyStroke({"cmd"}, "V", 0);
+    hs.eventtap.keyStroke({}, "Up", 0);
+    hs.eventtap.keyStroke({}, "Up", 0)
+    hs.eventtap.keyStroke({"cmd"}, "C", 0);
+    hs.eventtap.keyStroke({"cmd"}, "V", 0);
+    hs.eventtap.keyStroke({}, "Up", 0);
+    hs.eventtap.keyStroke({}, "Up", 0)
+end
