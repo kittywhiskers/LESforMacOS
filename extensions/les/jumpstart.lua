5f86da2e7ab313d20c17abd90ecb92f93dfd65ef
Kittywhiskers Van Gogh <63189531+kittywhiskers@users.noreply.github.com>
2023-05-21 18:50:08 +0530

    hsmodule: move accessibility state check to from jumpstart to init

--- extensions/les/jumpstart.lua
+++ extensions/les/jumpstart.lua
@@ -24,12 +24,5 @@ hs.automaticallyCheckForUpdates(false)
 
 bundlePath = hs.processInfo["bundlePath"]
 
-if hs.accessibilityState() == false then -- testing if hammerspoon has accessibility access.
-    hs.osascript.applescript(
-        [[tell application "System Events" to display dialog "Accessibility access is disabled which prevents LES from working properly." & return & "Please turn on accessibility access in" & return & "Preferences > Security & Privacy > Privacy > Accessibility, and try again." buttons {"Ok"} default button "Ok" with title "Live Enhancement Suite"]])
-    os.execute([[open /System/Library/PreferencePanes/Security.prefPane]])
-    os.exit()
-end
-
 package.path = package.path .. ";" .. bundlePath .. [[/Contents/Resources/extensions/hs/les/?.lua]]
 dofile(bundlePath .. [[/Contents/Resources/extensions/hs/les/LESmain.lua]])
