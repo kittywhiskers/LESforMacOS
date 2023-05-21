210d70ac6fe4a2aef4aeb32f2fd5ea53a1772666
Kittywhiskers Van Gogh <63189531+kittywhiskers@users.noreply.github.com>
2023-05-21 18:47:09 +0530

    hsmodule: add copyright notice in source files and create AUTHORS.txt

--- extensions/les/LESmain.lua
+++ extensions/les/LESmain.lua
@@ -1,3 +1,11 @@
+--  SPDX-License-Identifier: MIT
+--
+--  Copyright (c) 2019-2023 LESforMacOS authors, see AUTHORS.txt
+--  for a list
+--
+--  Distributed under the MIT software license, see the accompanying
+--  file COPYING.txt or visit https://opensource.org/license/mit/
+
 require("module")
 require("helpers")
 require("menus.keys.menu")
--- extensions/les/globals/constants.lua
+++ extensions/les/globals/constants.lua
@@ -1,2 +1,10 @@
+--  SPDX-License-Identifier: MIT
+--
+--  Copyright (c) 2019-2023 LESforMacOS authors, see AUTHORS.txt
+--  for a list
+--
+--  Distributed under the MIT software license, see the accompanying
+--  file COPYING.txt or visit https://opensource.org/license/mit/
+
 -- Program Name
 ProgramName = "Live Enhancement Suite"
