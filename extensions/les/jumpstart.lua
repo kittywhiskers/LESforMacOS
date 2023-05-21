6b5544344c668f890419be40b3fd3a16a04af721
Kittywhiskers Van Gogh <63189531+kittywhiskers@users.noreply.github.com>
2023-05-22 01:23:53 +0530

    hsmodule: move remaining jumpstart initialization logic to init, refine

--- extensions/les/jumpstart.lua
+++ extensions/les/jumpstart.lua
@@ -6,23 +6,6 @@
 --  Distributed under the MIT software license, see the accompanying
 --  file COPYING.txt or visit https://opensource.org/license/mit/
 
-console = hs.appfinder.windowFromWindowTitle("Hammerspoon Console")
-currentdir = hs.fs.currentDir()
-
-if console then
-    console:close()
-end -- attempts to close the console if it's up
-print(currentdir)
-
--- enables the dock icon while initializing
-hs.dockIcon(true)
-hs.menuIcon(false)
-hs.uploadCrashData(false)
-hs.openConsoleOnDockClick(true)
--- don't update hammerspoon and undo all my changes, please!
-hs.automaticallyCheckForUpdates(false)
-
 bundlePath = hs.processInfo["bundlePath"]
-
 package.path = package.path .. ";" .. bundlePath .. [[/Contents/Resources/extensions/hs/les/?.lua]]
 dofile(bundlePath .. [[/Contents/Resources/extensions/hs/les/LESmain.lua]])
